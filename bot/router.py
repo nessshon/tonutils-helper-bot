ec65b0b10d4559ee6c4c21a6044fd61d170ea165
nessshon <shonness98@gmail.com>
2024-10-19 20:15:58 +0500

    Add 'Getting Testnet Assets' article.

--- bot/router.py
+++ bot/router.py
@@ -50,6 +50,54 @@ async def create_article(cm: ContentManager, section: str, operation: str, link:
     )
 
 
+def get_testnet_assets_article() -> InlineQueryResultArticle:
+    operation = "Getting Testnet Assets"
+
+    message_text = (
+        f"{hlink('Tonutils', 'https://github.com/nessshon/tonutils')}\n\n"
+        f"🏷 {hbold(operation)}\n\n"
+        f" • {hbold('TON:')}\n"
+        f"<blockquote>Obtain testnet TON by interacting with the bot: "
+        f"{hlink('Open Bot', 'https://t.me/testgiver_ton_bot')}</blockquote>\n\n"
+        f" • {hbold('NOT Jettons')} (9 decimals):\n"
+        f"<blockquote>Claim testnet NOT by scanning the QR code at the following link: "
+        f"{hlink('Scan to Claim Testnet NOT', 'https://qrcode.ness.su/create?data=ton%3A%2F%2Ftransfer%2FkQBMfIaxfLQMP4h1Pg2V_AuyToC3jdB8MmA6u3bx8i1__NOT%3Famount%3D100000000%26bin%3Dte6ccgEBAgEAKgABIWQrfQcAAAAAAAAAABAX14QCAQAnF41FGQAAAAAAAAAAXo1KUQAAEBQ%3D&border=3&box_size=30&image_url=https%3A%2F%2Fcdn.joincommunity.xyz%2Fclicker%2Fnot_logo.png&image_round=50&image_padding=60')}</blockquote>\n\n"
+        f" • {hbold('USD₮ Jettons')}(6 decimals):\n"
+        f"<blockquote>Claim testnet USD₮ by scanning the QR code at the following link: "
+        f"{hlink('Scan to Claim Testnet USD₮', 'https://qrcode.ness.su/create?data=ton%3A%2F%2Ftransfer%2FkQB0ZYUL5M3KfrW0tSnwdFO1nC-BQHC2gcZl-WaF2on_USDT%3Famount%3D100000000%26bin%3Dte6ccgEBAgEAKQABIWQrfQcAAAAAAAAAABAX14QCAQAlF41FGQAAAAAAAAAAQ7msoAAQFA%3D%3D&border=3&box_size=30&image_url=https%3A%2F%2Ftether.to%2Fimages%2FlogoCircle.png&image_round=50&image_padding=99')}</blockquote>\n\n"
+        f"⚠️ {hbold('Note:')} Make sure to use a wallet configured for the testnet to scan the code and confirm the claim. Wait for the transaction to complete, after which the tokens will appear in your wallet!"
+    )
+    reply_markup = InlineKeyboardMarkup(
+        inline_keyboard=[
+            [
+                InlineKeyboardButton(
+                    text="Get TON",
+                    url="https://t.me/testgiver_ton_bot"
+                ),
+                InlineKeyboardButton(
+                    text="Get NOT",
+                    url="ton://transfer/kQBMfIaxfLQMP4h1Pg2V_AuyToC3jdB8MmA6u3bx8i1__NOT?amount=100000000&bin=te6ccgEBAgEAKgABIWQrfQcAAAAAAAAAABAX14QCAQAnF41FGQAAAAAAAAAAXo1KUQAAEBQ="
+                ),
+                InlineKeyboardButton(
+                    text="Get USD₮",
+                    url="ton://transfer/kQB0ZYUL5M3KfrW0tSnwdFO1nC-BQHC2gcZl-WaF2on_USDT?amount=100000000&bin=te6ccgEBAgEAKQABIWQrfQcAAAAAAAAAABAX14QCAQAlF41FGQAAAAAAAAAAQ7msoAAQFA=="
+                ),
+            ],
+        ]
+    )
+    return InlineQueryResultArticle(
+        id=hashlib.sha256(f"{operation}1231".encode()).hexdigest(),
+        title=operation,
+        thumbnail_url="https://telegra.ph//file/063ee61e097ee8281b9d3.jpg",
+        input_message_content=InputTextMessageContent(
+            message_text=message_text,
+            disable_web_page_preview=True,
+        ),
+        reply_markup=reply_markup,
+    )
+
+
+
 @router.inline_query()
 async def search(inline: InlineQuery) -> None:
     items_per_page = 20
@@ -60,6 +108,8 @@ async def search(inline: InlineQuery) -> None:
 
     if any(items):
         results = []
+        if offset == 0:
+            results.append(get_testnet_assets_article())
 
         for section, operation, link in items[offset:offset + items_per_page]:
             result = await create_article(cm, section, operation, link)
